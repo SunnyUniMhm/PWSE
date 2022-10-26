0446e0624a3406a1ae987489dcf33a56c457a565
SunnyUniMhm <sunny_flower_field_yep@protonmail.com>
2022-10-26 09:10:14 +0300

    Add simple class for main game functionality

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,62 @@
+/* jslint browser: true */
+/* jslint es6: true */
+/* global $, jQuery, document */
+/* eslint-env jquery */
+/* eslint-env es6 */
+
 $(document).ready(function () {
-    /* eslint-env jquery */
     "use strict";
     
-    function createGameField() {
-        const mainGameField = $('#main-game-field');
+    class MainGame {
+        totalScore;
+        maxTileScore;
+        freeCellsLeft;
+        tilesData;
+        #mainGameField;
         
-        for (let rowNumber = 1; rowNumber <= 4; rowNumber ++) {
-            for (let cellNumber = 1; cellNumber <= 4; cellNumber ++) {
-                mainGameField.append(`<div id="cell-${rowNumber}-${cellNumber}" class="rounded text-center fw-bold fs-2"></div>`);
+        constructor() {
+            this.totalScore = 0;
+            this.maxTileScore = 0;
+            this.freeCellsLeft = 16;
+            this.tilesData = {
+                1: {
+                    1: null,
+                    2: null,
+                    3: null,
+                    4: null,
+                },
+                2: {
+                    1: null,
+                    2: null,
+                    3: null,
+                    4: null,
+                },
+                3: {
+                    1: null,
+                    2: null,
+                    3: null,
+                    4: null,
+                },
+                4: {
+                    1: null,
+                    2: null,
+                    3: null,
+                    4: null,
+                }
+            };
+            this.#mainGameField = $('#main-game-field');
+        }
+
+        createGameField() {
+            for (let rowNumber = 1; rowNumber <= 4; rowNumber ++) {
+                for (let cellNumber = 1; cellNumber <= 4; cellNumber ++) {
+                    this.#mainGameField.append(`<div id="cell-${rowNumber}-${cellNumber}" class="rounded text-center fw-bold fs-2"></div>`);
+                }
             }
         }
     }
-    
-    createGameField();
+
+    var game = new MainGame();
+    console.log(game);
+    game.createGameField();
 });
